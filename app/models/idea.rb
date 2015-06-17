98e7cfac6e26d1e355c05ab9f15e61b0bb0db415
Angelica Carlos <angelica@aelogica.com>
2015-06-17 10:02:38 +0800

    updated comments relationship to ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ActiveRecord::Base
-mount_uploader :picture, PictureUploader
+	has_many :comments
+	mount_uploader :picture, PictureUploader
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  devise_for :users
   get 'pages/info'
 
   root :to => redirect('/ideas')
