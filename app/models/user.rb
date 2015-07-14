3a9d233b329db7c4ea7fda95680f2e30dbd43e9e
Jace Monje <jace@aelogica.com>
2015-07-14 12:37:49 +0800

    reworked home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-has_many :user_feedbacks
+          :rememberable, :trackable, :validatable
+
+  has_many :user_feedbacks
 
 end
