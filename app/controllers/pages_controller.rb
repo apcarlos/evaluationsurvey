386805d244aa447264db437a555775d7e89f14d6
Jace Monje <jace@aelogica.com>
2015-07-16 12:17:54 +0800

    fixed accesses and privileges

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
 
 	def home
-		
+		if current_user
+			if current_user.admin
+				@feedbacks=UserFeedback.all
+			else
+				@feedbacks=current_user.user_feedbacks
+			end
+		end
+
 	end
 
 	def info
